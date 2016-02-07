0554e020281a196b57f3a73eb314e0f74a59d4bc
toast38coza <info@38.co.za>
2016-02-07 22:53:43 +0200

    Adds a health endpoint

--- api/tests.py
+++ api/tests.py
@@ -1,8 +1,45 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User, AnonymousUser
 from django.core.urlresolvers import reverse
+from django.db import DatabaseError
 import json
+from mock import patch
 from rest_framework.test import APIClient
+
+class HealthTestCase(TestCase):
+    def setUp(self):
+        self.c = APIClient()
+        self.status_fields = ['db', 'status']
+
+    def test_health_endpoint_ok(self):
+        url = reverse('health-list')
+        response = self.c.get(url)
+        assert response.status_code == 200, \
+            "Expect 200 OK. got: {}" . format (response.status_code)
+
+        expected_fields = ["db", "status"]
+
+        for field in self.status_fields:
+            assert response.json().get("status", {}).get(field, None) == "up", \
+                "Expected field {} to exist" . format (field)
+
+    @patch.object(User.objects, 'first')
+    def test_determine_db_status(self, mock_query):
+        """Health should not be ok if it cannot connect to the db"""
+
+        mock_query.side_effect = DatabaseError()
+        url = reverse('health-list')
+        response = self.c.get(url)
+
+        status = response.json().get("status", {})
+        db_status = status.get('db')
+        assert db_status == 'down', \
+            'Expect DB to be down. Got: {}' . format (db_status)
+
+        status = status.get('status')        
+        assert status == 'down', \
+            'Expect status to be down. Got: {}' . format (status)
+        
 
 
 class UserAPITestCase(TestCase):
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers, serializers, viewsets, decorators, response
 from api.permissions import IsSelfOrSuperUser
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 # Serializers define the API representation.
 
 
@@ -20,8 +20,44 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     permission_classes = (IsSelfOrSuperUser, )
 
+class HealthViewSet(viewsets.ViewSet):
 
+	base_name = 'health'
+	permission_classes = (AllowAny, )
+
+	def list(self, request, format=None):
+
+		## make sure we can connect to the database
+		all_statuses = []
+		status = "up"
+		
+		db_status = self.__can_connect_to_db()
+
+		all_statuses.append(db_status)
+
+		if "down" in all_statuses:
+			status = "down"
+
+		data = {
+			"data": {
+				"explorer" : "/api-explorer",
+			},
+			"status": {
+				"db": db_status,
+				"status": status 
+			}
+		}
+		return response.Response(data)	
+
+	def __can_connect_to_db(self):
+		try:
+			user = User.objects.first()	
+			return "up"
+		except Exception:
+			return "down"
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
+router.register(r'health', HealthViewSet, base_name='health')
 router.register(r'users', UserViewSet)
+
